6b31b06f73af3aae4adf10ad943e22c87fc6988e
sakshishewale <sakshishewale2808@gmail.com>
2024-07-19 15:11:39 +0530

    connection of mongodb and controllers

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { getHealth } from "./controllers/data.js";
-
 dotenv.config();
+import mongoose from "mongoose";
 
 import { postPlant,
     getPlants ,
@@ -15,6 +15,19 @@ import {error} from "./controllers/error.js";
 const app = express();
 app.use(express.json());
 
+const dbConnection = async ()=>{
+    const conn = await mongoose.connect(process.env.MONGO_URL)
+
+    if(conn){
+        console.log(`Mongodb connected`)
+    }
+    else{
+        console.log(`Mongodb not connected`)
+    }
+
+}
+dbConnection();
+
 app.get("/plants",getPlants);
 app.get("/plant/:id",getPlantId);
 app.post("/plant",postPlant);
@@ -22,7 +35,6 @@ app.put("/plant/:id", putPlantId);
 app.delete("/plant/:id",deletePlantId);
 
 app.get("/health", getHealth);
-
 app.use("*", error);
 
 const PORT = process.env.PORT || 8000;
